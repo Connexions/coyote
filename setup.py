61e4019b989f9b892dd7594fe8ee289eff4d0f84
Michael Mulich <michael.mulich@gmail.com>
2012-12-07 22:14:14 -0500

    It's a distribute with a package in it.

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = os.path.join(here, 'README.rst')
+
+setup(
+    name='rbit',
+    version='1.0',
+    author="Connexions/Rhaptos Team",
+    author_email="info@cnx.org",
+    description='Rhaptos PyBit client implementation',
+    long_description=open(README).read(),
+    url='https://github.com/connexions/rbit',
+    license='GPL2',  # See also LICENSE.txt
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'pybit',
+        ],
+    entry_points = """\
+    """,
+    )

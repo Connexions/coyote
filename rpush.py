c05a3c27f539700c0fa3ef842e1796f54f32abd1
Michael Mulich <michael.mulich@gmail.com>
2013-01-24 08:42:03 -0500

    long winded draft that does the complete job creation process.

--- rpush.py
+++ rpush.py
@@ -11,79 +11,26 @@ https://github.com/nicholasdavidson/pybit licensed under GPL2.1.
 This software is subject to the provisions of the GNU Lesser General
 Public License Version 2.1 (LGPL).  See LICENSE.txt for details.
 """
-import sys
+import json
 import argparse
 import requests
 
 
-def add_package(args):
-    """Adds a package to PyBit"""
-    url = "http://{0}:{1}/package/".format(args.host, args.port)
-    auth = (args.user, args.password,)
-    data = {'name': args.id, 'version': args.version}
-    resp = requests.post(url, data=data, auth=auth)
-    if resp.status_code != 200:
-        raise Exception("Failed to add the package with the following data:"
-                        "\ndata: {0}"
-                        "\nresponse status: {1}"
-                        "\nresponse body: {2}"
-                        .format(data, resp.status_code, resp.text))
-
-
-def _name2id(lookup_name, name, args):
-    """Map the lookup name to an id"""
-    url = "http://{0}:{1}/{2}/page/1".format(args.host, args.port, lookup_name)
-    auth = (args.user, args.password,)
-    resp = requests.get(url, auth=auth)
-    data = dict([(v['name'], v['id'],) for v in resp.json])
-    return data[name]
-
-
-def add_instance(args):
-    """Adds a package instance to PyBit"""
-    url = "http://{0}:{1}/packageinstance/".format(args.host, args.port)
+def create_job(args):
+    """Creates a job in PyBit"""
+    url = "http://{0}:{1}/api/job/".format(args.host, args.port)
     auth = (args.user, args.password,)
     data = {'package': args.id,
             'version': args.version,
-            'arch_id': _name2id('arch', args.platform, args),
-            'suite_id': _name2id('suite', args.suite, args),
-            'dist_id': _name2id('dist', args.project, args),
-            'format_id': _name2id('format', args.format, args),
-            }
-    resp = requests.post(url, data=data, auth=auth)
-    if resp.status_code != 200:
-        raise Exception("Failed to add the package with the following data:"
-                        "\ndata: {0}"
-                        "\nresponse status: {1}"
-                        "\nresponse body: {2}"
-                        .format(data, resp.status_code, resp.text))
-
-
-def _packageinstance2id(name, version, args):
-    """Lookup name in the package instance listing to get its id mapping."""
-    url = "http://{0}:{1}/packageinstance/page/1".format(args.host, args.port)
-    auth = (args.user, args.password,)
-    resp = requests.get(url, auth=auth)
-    data = dict([(v['package']['version'], v['package']['id'],)
-                 for v in resp.json
-                 if v['package']['name'] == unicode(name)
-                 ])
-    return data[unicode(version)]
-
-
-def add_job(args):
-    """Adds a job to PyBit"""
-    url = "http://{0}:{1}/job/".format(args.host, args.port)
-    auth = (args.user, args.password,)
-    data = {'package': args.id,
-            'packageinstance_id': _packageinstance2id(args.id, args.version,
-                                                      args),
             'uri': args.uri,
-            'method': '',
-            'vcs_id': '',
-            'commands': '',
+            'arch': args.platform,
+            'dist': args.project,
+            'suite': args.suite,
+            'format': args.format,
             }
-    resp = requests.post(url, data=data, auth=auth)
+    data = json.dumps(data)
+    headers = {'content-type': 'application/json'}
+    resp = requests.post(url, data=data, auth=auth, headers=headers)
     if resp.status_code != 200:
         raise Exception("Failed to add the package with the following data:"
                         "\ndata: {0}"
@@ -96,6 +43,8 @@ def add_job(args):
 def main(argv=None):
     """Command line utility"""
     parser = argparse.ArgumentParser(description="PyBit builder for rhaptos")
+
+    # PyBit connection information
     parser.add_argument('--host', default='localhost',
                         help="PyBit web frontend hostname")
     parser.add_argument('--port', type=int, default=8080,
@@ -104,44 +53,23 @@ def main(argv=None):
                         help="Username for auth against PyBit")
     parser.add_argument('--password', default='pass',
                         help="password for auth against PyBit")
-    # Subcommands: package, instance, and job
-    subparsers = parser.add_subparsers()
 
-    package_parser = subparsers.add_parser('package',
-                                           help="adds a module/collection")
-    package_parser.set_defaults(func=add_package)
-    package_parser.add_argument('id', help="module/collection id")
-    package_parser.add_argument('version',
-                                help="version of the module/collection")
-
-    instance_parser = subparsers.add_parser('instance',
-                                            help="adds a module/collection "\
-                                                "instance")
-    instance_parser.set_defaults(func=add_instance)
-    instance_parser.add_argument('id', help="module/collection id")
-    instance_parser.add_argument('version',
-                                 help="version of the module/collection")
-    instance_parser.add_argument('--platform', default='any',
-                                 help="platform to build for, default is any")
-    instance_parser.add_argument('--suite', default='latex',
-                                 help="build suite to use, default is latex")
-    instance_parser.add_argument('--project', default='cnx',
-                                 help="build for a specific project, default "\
-                                      "is cnx")
-    instance_parser.add_argument('--format', default='pdf',
-                                 help="build format, default is pdf")
-
-    job_parser = subparsers.add_parser('job',
-                                       help="adds a job to build a "\
-                                           "module/collection")
-    job_parser.set_defaults(func=add_job)
-    job_parser.add_argument('id', help="module/collection id")
-    job_parser.add_argument('version',
+    # Job information
+    parser.add_argument('id', help="module/collection id")
+    parser.add_argument('version',
                             help="version of the module/collection")
-    job_parser.add_argument('uri', help="URI to the module/collection")
+    parser.add_argument('uri', help="URI to the module/collection")
+    parser.add_argument('--platform', default='any',
+                        help="platform to build for, default is any")
+    parser.add_argument('--suite', default='latex',
+                        help="build suite to use, default is latex")
+    parser.add_argument('--project', default='cnx',
+                        help="build for a specific project, default is cnx")
+    parser.add_argument('--format', default='pdf',
+                        help="build format, default is pdf")
 
     args = parser.parse_args(argv)
-    args.func(args)
+    create_job(args)
 
 
 if __name__ == '__main__':

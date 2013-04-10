8fa35a044bc75a319dbf272e317c6a686ec9939e
Ed Woodward <ecw1@rice.edu>
2013-04-10 12:51:44 -0500

    Added license info

--- rbit.py
+++ rbit.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""\
+"""
 Pybit client implemenation for Rhaptos workers.
 
 Author: Michael Mulich
@@ -8,8 +8,8 @@ Copyright (c) 2012 Rice University
 Parts of the client code are derived from the PyBit client implementation at
 https://github.com/nicholasdavidson/pybit licensed under GPL2.1.
 
-This software is subject to the provisions of the GNU Lesser General
-Public License Version 2.1 (LGPL).  See LICENSE.txt for details.
+This software is subject to the provisions of the GNU AFFERO GENERAL PUBLIC LICENSE Version 3.0 (AGPL).  
+See LICENSE.txt for details.
 """
 import time
 import logging
--- tests.py
+++ tests.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2013 Rice University
+
+This software is subject to the provisions of the GNU AFFERO GENERAL PUBLIC LICENSE Version 3.0 (AGPL).  
+See LICENSE.txt for details.
+"""
+
 """Functional tests for the `rbit`` implementation.
 
 DO NOT have a production (rabbitmq) message queue running while running these

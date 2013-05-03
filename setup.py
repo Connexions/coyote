99029f55c493a1e3fafaa72af99ef2ca7174e1f3
Michael Mulich <michael.mulich@gmail.com>
2013-05-03 11:53:56 -0400

    Rename rbit to coyote

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
 # -*- coding: utf-8 -*-
-
-"""
-Copyright (C) 2013 Rice University
-
-This software is subject to the provisions of the GNU AFFERO GENERAL PUBLIC LICENSE Version 3.0 (AGPL).  
-See LICENSE.txt for details.
-"""
-
 import os
 from setuptools import setup, find_packages
 
@@ -14,24 +6,26 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = os.path.join(here, 'README.rst')
 
 install_requirements = [
-    'pybit',
     'pika',
-    'jsonpickle',
     'requests',
+    # PyBit and dependencies
+    'pybit',
+    'psycopg2',
+    'amqplib',
+    'jsonpickle',
     ]
 test_requirements = [
-    'mock',
     ]
 
 setup(
     name='rbit',
-    version='1.0',
+    version='0.1',
     author="Connexions/Rhaptos Team",
     author_email="info@cnx.org",
-    description='Rhaptos PyBit client implementation',
+    description='',
     long_description=open(README).read(),
-    url='https://github.com/connexions/rbit',
-    license='GPL2',  # See also LICENSE.txt
+    url='https://github.com/connexions/coyote',
+    license='AGPL',  # See also LICENSE.txt
     packages=find_packages(),
     include_package_data=True,
     install_requires=install_requirements,
@@ -40,6 +34,6 @@ setup(
         },
     entry_points = """\
     [console_scripts]
-    rbit = rbit:main
+    coyote = coyote:main
     """,
     )
